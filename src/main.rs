b1f43bd25fd48f8f7dbf6631e03a3ca597412f01
Nathaniel <nathaniels2016@gmail.com>
2020-02-23 15:21:43 -0800

    Jump

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,39 @@
+
 use ggez;
-use ggez::event;
+use ggez::event::{self, EventHandler, KeyCode, KeyMods};
 use ggez::graphics;
+use ggez::input::keyboard;
 use ggez::nalgebra as na;
+
 
 struct MainState {
     pos_x: f32,
+    pos_y: f32,
+    jump: bool,
 }
 
 impl MainState {
     fn new() -> ggez::GameResult<MainState> {
-        let s = MainState { pos_x: 0.0 };
+        let s = MainState { pos_x: 0.0, pos_y:400.0, jump:false,};
         Ok(s)
     }
 }
 
 impl event::EventHandler for MainState {
     fn update(&mut self, _ctx: &mut ggez::Context) -> ggez::GameResult {
-        self.pos_x = self.pos_x % 800.0 + 1.0;
+        if self.jump {
+            self.jump = false;
+            self.pos_y = self.pos_y + 100.0;
+        }  
+        if keyboard::is_key_pressed(_ctx, KeyCode::Space) {
+            self.pos_y = self.pos_y - 100.0;
+            self.jump = true;
+        }
+        if self.pos_x < 750.0  {
+            self.pos_x = self.pos_x + 0.5;
+        }
+
+
         Ok(())
     }
 
@@ -26,10 +43,10 @@ impl event::EventHandler for MainState {
         let circle = graphics::Mesh::new_circle(
             ctx,
             graphics::DrawMode::fill(),
-            na::Point2::new(self.pos_x, 380.0),
-            100.0,
+            na::Point2::new(self.pos_x, self.pos_y),
+            50.0,
             2.0,
-            graphics::WHITE,
+            graphics::BLACK,
         )?;
         graphics::draw(ctx, &circle, (na::Point2::new(0.0, 0.0),))?;
 

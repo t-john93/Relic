8d20b0a0f73e5a3386500940e3962520dfb0cb37
t-john93 <tjohnson1293@gmail.com>
2020-03-03 22:36:09 -0800

    Finished engine module and created place to add map polygons in game state

--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -1,16 +1,55 @@
-pub get_x_pos(&mut state: MainState) {
-    state.x_pos += state.x_velocity;
-    state.y_velocity += state.gravity;
-
-    if self.x_pos < 50.0 || self.x_pos > 750.0 { self.x_velocity *= -1.0 }
+pub struct EngineState {
+    pub x_pos: f32,
+    pub y_pos: f32,
+    pub x_velocity: f32,
+    pub y_velocity: f32,
+    pub gravity: f32,
+    pub grounded: bool,
 }
 
-pub get_y_pos(&mut y_pos: f32, &mut y_velocity, &mut gravity: f32, &mut grounded: bool) {
-        if y_pos > 500.0 {
-            y_pos = 500.0;
-            y_velocity = 0.0;
-            grounded = true;
+impl EngineState {
+    pub fn new() -> EngineState {
+        let es = EngineState {
+            x_pos: 0.0,
+            y_pos: 0.0,
+            x_velocity: 0.0,
+            y_velocity: 0.0,
+            gravity: 0.0,
+            grounded: false,
+        };
+        es
+    }
+    pub fn construct_new(x: f32,y: f32,xv: f32,yv: f32,g: f32,on_ground: bool) -> EngineState {
+        let es = EngineState {
+            x_pos: x,
+            y_pos: y,
+            x_velocity: xv,
+            y_velocity: yv,
+            gravity: g,
+            grounded: on_ground,
+        };
+        es
+    }
+    pub fn get_x_pos(&mut self) {
+        self.x_pos += self.x_velocity;
+        self.y_velocity += self.gravity;
+        if self.x_pos < 50.0 || self.x_pos > 750.0 { self.x_velocity *= -1.0; }
+    }
+    pub fn get_y_pos(&mut self) {
+        if self.check_ground() {
+            self.y_pos = 500.0;
+            self.y_velocity = 0.0;
         }
-
-        y_pos += y_velocity;
+        self.y_pos += self.y_velocity;
+    }
+    pub fn check_ground(&mut self) -> bool {
+        if self.y_pos > 500.0 { self.grounded = true; }
+        self.grounded
+    }
+    // pub fn set_y_velocity(&mut self, velocity: f32) {
+    //     self.y_velocity = velocity;
+    // }
+    // pub fn set_grounded(&mut self, on_ground: bool) {
+    //     self.grounded = on_ground;
+    // }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,40 @@
 use ggez::{Context, ContextBuilder, GameResult, graphics, event, nalgebra as na};
 use event::{EventHandler, run, KeyCode, KeyMods};
-use graphics::Rect;
 // use conf::{WindowSetup};
 use std::env;
 use std::path;
 
-// mod engine;
+mod engine;
 
-struct MainState {
-    x_pos: f32,
-    y_pos: f32,
-    x_velocity: f32,
-    y_velocity: f32,
-    gravity: f32,
-    grounded: bool,
+pub struct PlayerState {
+    player_physics: engine::EngineState,
+    // map_model: [graphics::Rect; 20],     // TODO return array of map polygons
+    map_model: graphics::Rect,
 }
 
-impl MainState {
-    fn new() -> GameResult<MainState> {
-        let s = MainState {x_pos: 100.0, y_pos: 200.0, x_velocity: 3.0, y_velocity: 0.0, gravity: 0.5, grounded: false};
+impl PlayerState {
+    fn new() -> GameResult<PlayerState> {
+        let s = PlayerState {
+            player_physics: engine::EngineState::construct_new(100.0, 200.0, 3.0, 0.0, 0.5, false),
+            map_model: graphics::Rect::new(0.0, 520.0, 800.0, 100.0),
+        };
         Ok(s)
     }
 }
 
-impl EventHandler for MainState {
-
+impl EventHandler for PlayerState {
     fn update(&mut self, _ctx: &mut Context) -> GameResult {
-        self.x_pos += self.x_velocity;
-        self.y_velocity += self.gravity;
-        if self.y_pos > 500.0 {
-            self.y_pos = 500.0;
-            self.y_velocity = 0.0;
-            self.grounded = true;
-        }
-        self.y_pos += self.y_velocity;
-        if self.x_pos < 50.0 || self.x_pos > 750.0 { self.x_velocity *= -1.0 }
-
-        // BROKEN
-        // engine::get_x_pos(&self);
-        // engine::get_y_pos(self.y_pos, self.y_velocity, self.gravity, self.grounded);
-
+        self.player_physics.get_x_pos();
+        self.player_physics.get_y_pos();
+        self.player_physics.check_ground();
         Ok(())
     }
-
     fn key_down_event(&mut self, ctx: &mut Context, keycode: KeyCode, _keymod: KeyMods, _repeat: bool) {
         match keycode {
             KeyCode::Space => {
-
-                if self.grounded {
-                    self.y_velocity = -12.0;
-                    self.grounded = false;
+                if self.player_physics.check_ground() {
+                    self.player_physics.y_velocity = -12.0;
+                    self.player_physics.grounded = false;
                 }
             }
             KeyCode::Escape => {
@@ -61,14 +46,10 @@ impl EventHandler for MainState {
     
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
         graphics::clear(ctx, [0.1, 0.2, 0.3, 1.0].into());
-
-        // let ground = Rect::zero();
-        let ground_rec = Rect::new(0.0, 520.0, 800.0, 100.0);
-
         let circle = graphics::Mesh::new_circle(
             ctx,
             graphics::DrawMode::fill(),
-            na::Point2::new(self.x_pos, self.y_pos),
+            na::Point2::new(self.player_physics.x_pos, self.player_physics.y_pos),
             20.0,
             0.5,
             graphics::BLACK,
@@ -76,10 +57,11 @@ impl EventHandler for MainState {
         let ground = graphics::Mesh::new_rectangle(
             ctx,
             graphics::DrawMode::fill(),
-            ground_rec,
+            self.map_model,
             graphics::WHITE
         )?;
         graphics::draw(ctx, &circle, (na::Point2::new(0.0, 0.0),))?;
+        graphics::draw(ctx, &ground, (na::Point2::new(0.0, 0.0),))?;
         graphics::draw(ctx, &ground, (na::Point2::new(0.0, 0.0),))?;
 
         graphics::present(ctx)?;
@@ -99,6 +81,6 @@ pub fn main() -> GameResult {
 
     let cb = ContextBuilder::new("Relic", "ggez").add_resource_path(resource_dir);
     let (ctx, event_loop) = &mut cb.build()?;
-    let state = &mut MainState::new()?;
+    let state = &mut PlayerState::new()?;
     run(ctx, event_loop, state)
 }
